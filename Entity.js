c0b068e88b71332872826ca4e374eca10cfc3f2e
Evgeny <hatber.index@gmail.com>
2023-08-01 17:53:16 +0900

    add diagonal move directions and other animations

--- Entity.js
+++ Entity.js
@@ -1,9 +1,10 @@
-export class Sprite {
-  constructor(x, y, width, height, speed) {
+export class Entity {
+  constructor(x, y, width, height, speed, animation) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.speed = speed;
+    this.animation = animation;
   }
 }
--- render.js
+++ render.js
@@ -1,84 +1,231 @@
 import { canvas, ctx, canvasHeight, canvasWidth } from "./cavnas";
-import { Sprite } from "./sprite";
-import { playerSprite } from "./playerSprite";
+import { Entity } from "./Entity";
 
-const player = new Sprite(canvasWidth/ 2, canvasHeight/ 2, 50, 50, 10);
+const player = new Entity(
+  canvasWidth / 2,
+  canvasHeight / 2,
+  50,
+  50,
+  10,
+  "iddle"
+);
 
-let spriteX = 9;
-let spriteY = 14;
+let spriteX = 4;
+let spriteY = 7;
+let runUpX = 0;
+let runUpY = 0;
+let aInterval = 0;
 
-export const render = function() {
+const iddleSpriteImg = document.createElement("img");
+iddleSpriteImg.setAttribute("src", "./level1_idle.png");
+iddleSpriteImg.style.display = "none";
+
+const runUpSpriteImg = document.createElement("img");
+runUpSpriteImg.setAttribute("src", "level1_running.png");
+runUpSpriteImg.style.display = "none";
+
+const terrainImg = document.createElement("img");
+terrainImg.setAttribute("src", "./slice.png");
+terrainImg.style.display = "none";
+
+export const render = function () {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
   ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+  const pattern = ctx.createPattern(terrainImg, "repeat");
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
   ctx.strokeStyle = "#FFFFFF";
   ctx.strokeText(`player y: ${player.y}`, 50, 50);
 
-  ctx.fillStyle = "#FFFFFF";
-  // ctx.fillRect(player.x, player.y, player.width, player.height);
-  ctx.drawImage(playerSprite, spriteX, spriteY, 61, 99, player.x, player.y, 61, 99);
-}
+  ctx.strokeStyle = "#FFFFFF";
+  ctx.strokeText(`iddle: ${player.animation}`, 50, 70);
 
-let iddling = false;
-let runUp = false;
+  if (player.animation === "iddle") {
+    ctx.drawImage(
+      iddleSpriteImg,
+      spriteX,
+      spriteY,
+      31,
+      50,
+      player.x,
+      player.y,
+      31,
+      50
+    );
+  } else if (player.animation === "runUp") {
+    ctx.drawImage(
+      runUpSpriteImg,
+      runUpX,
+      runUpY,
+      45,
+      62,
+      player.x,
+      player.y,
+      45,
+      62
+    );
+  }
+};
 
-// Idling
-if (iddling === true) {
-  setInterval(() => {
-    function iddling() {
-      console.log(spriteX);
-      spriteX += 92;
-      if (spriteX === 1941) {
-        spriteX = 9;
+setInterval(() => {
+  if (player.animation === "iddle") {
+    spriteX += 46;
+    if (spriteX === 970) {
+      spriteX = 4;
+    }
+  }
+}, 150);
+
+setInterval(() => {
+  if (player.animation === "runUp") {
+    runUpX += 44;
+    if (runUpX === 924) {
+      runUpX = 0;
+    }
+  }
+}, 30);
+
+// setInterval(() => {
+//   if (player.animation === "runDown") {
+//     runUpX += 44;
+//     if (runUpX === 924) {
+//       runUpX = 0;
+//     }
+//   }
+// }, 30);
+
+let pressed = new Set();
+
+window.addEventListener("keydown", (event) => {
+  pressed.add(event.key);
+  if (!aInterval) {
+    aInterval = setInterval(() => {
+      switch (true) {
+        case (pressed.has("ArrowUp") && pressed.has("ArrowLeft")):
+          player.animation = "runUp";
+          runUpY = 462;
+          player.y -= player.speed / 1.5;
+          player.x -= player.speed / 1.5;
+          break;
+        case (pressed.has("ArrowUp") && pressed.has("ArrowRight")):
+          player.animation = "runUp";
+          runUpY = 65;
+          player.y -= player.speed / 1.5;
+          player.x += player.speed / 1.5;
+          break;
+        case (pressed.has("ArrowDown") && pressed.has("ArrowRight")):
+          player.animation = "runUp";
+          runUpY = 201;
+          player.y += player.speed / 1.5;
+          player.x += player.speed / 1.5;
+          break;
+        case (pressed.has("ArrowDown") && pressed.has("ArrowLeft")):
+          player.animation = "runUp";
+          runUpY = 331;
+          player.y += player.speed / 1.5;
+          player.x -= player.speed / 1.5;
+          break;
+        case (pressed.has("ArrowUp")):
+          player.animation = "runUp";
+          player.y -= player.speed;
+          break;
+          case (pressed.has("ArrowLeft")):
+          player.animation = "runUp";
+          runUpY = 397;
+          player.x -= player.speed;
+          break;
+        case (pressed.has("ArrowRight")):
+          player.animation = "runUp";
+          runUpY = 130;
+          player.x += player.speed;
+          break;
+        case (pressed.has("ArrowDown")):
+          player.animation = "runUp";
+          runUpY = 266;
+          player.y += player.speed;
+          break;
       }
-    }
-    iddling()
-  }, 180)
-}
-
-if (runUp === true) {
-  setInterval(() => {
-    function iddling() {
-      console.log(spriteX);
-      spriteX += 92;
-      if (spriteX === 1941) {
-        spriteX = 9;
-      }
-    }
-    iddling()
-  }, 180)
-}
-
-window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowUp") {
-    if (player.y > 0) {
-      player.y -= player.speed;
-    }
+    }, 30);
   }
 });
 
-window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowDown") {
-    if (player.y < canvasHeight) {
-      player.y += player.speed;
-    }
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      pressed.delete("ArrowUp");
+      player.animation = "iddle";
+      spriteY = 7;
+      break;
+    case "ArrowDown":
+      pressed.delete("ArrowDown");
+      player.animation = "iddle";
+      runUpY = 0;
+      spriteY = 232;
+      break;
+    case "ArrowLeft":
+      pressed.delete("ArrowLeft");
+      player.animation = "iddle";
+      runUpY = 0;
+      spriteY = 349;
+      break;
+    case "ArrowRight":
+      player.animation = "iddle";
+      runUpY = 0;
+      spriteY = 117;
+      pressed.delete("ArrowRight");
+      break;
+    default:
+      pressed.clear();
   }
 });
 
-window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") {
-    if (player.x > 0) {
-      player.x -= player.speed;
-    }
-  }
-});
+// window.addEventListener("keydown", (event) => {
+//   if (event.key === "ArrowUp") {
+//     if (!aInterval) {
+//       aInterval = setInterval(function() {
+//         if (player.y > 0) {
+//           player.y -= player.speed;
+//           player.animation = "run";
+//         }
+//       }, 30);
+//     }
+//   }
+// });
 
-window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowRight") {
-    if (player.x < canvasWidth) {
-      player.x += player.speed;
-    }
-  }
-});
+// window.addEventListener("keyup", (event) => {
+//   if (aInterval) {
+//     clearInterval(aInterval);
+//     aInterval = 0;
+//     player.animation = "iddle";
+//   }
+// })
+
+// window.addEventListener("keydown", (event) => {
+//   if (event.key === "ArrowDown") {
+//     if (player.y < canvasHeight) {
+//       player.y += player.speed;
+//     }
+//   }
+// });
+
+// window.addEventListener("keydown", (event) => {
+//   if (event.key === "ArrowLeft") {
+//     if (player.x > 0) {
+//       player.x -= player.speed;
+//     }
+//   }
+// });
+
+// window.addEventListener("keydown", (event) => {
+//   if (event.key === "ArrowRight") {
+//     if (player.x < canvasWidth) {
+//       player.x += player.speed;
+//     }
+//   }
+// });
+
+// startAnimation();
